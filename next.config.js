428ed38e7a599a807f62adb6e1ff6ff673d57842
Sandrino Di Mattia <contact@sandrino.be>
2019-09-12 22:20:08 +0200

    Stop using runtime config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const { withAuth0 } = require('@auth0/nextjs-auth0');
 dotenv.config();
 
 module.exports = withAuth0({
-  serverRuntimeConfig: {
+  env: {
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
